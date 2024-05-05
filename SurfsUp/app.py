1c7c9df2042a29c5906657b6b51d9b5e1577289e
mpeck34 <mark.peck34@gmail.com>
2024-05-05 17:33:54 +1000

    trouble opening sqlite

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,14 @@
 # Import the dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,26 +16,61 @@
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Declare a Base using `automap_base()`
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
+Base.prepare(engine, reflect=True)
 
+print(Base.metadata.tables.keys())
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/measurements<br/>"
+        f"/api/v1.0/stations"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    results = session.query(Measurement.station, Measurement.date, Measurement.prcp).all()
+    session.close()
+
+    precipitation_list = []
+    for station, date, prcp in results:
+        precip_dict = {
+            "station": station,
+            "date": date,
+            "prcp": prcp
+        }
+        precipitation_list.append(precip_dict)
+
+    return jsonify(precipitation_list)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
